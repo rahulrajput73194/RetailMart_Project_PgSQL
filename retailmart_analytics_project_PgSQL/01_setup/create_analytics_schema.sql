ee5041a57e175a936b937a63c3ce65ed39870da6
Rahul <rahul73194@gmail.com>
2025-12-28 18:25:52 +0530

    final commit project completed

--- retailmart_analytics_project_PgSQL/01_setup/create_analytics_schema.sql
+++
@@ -1,2 +0,0 @@
---creating the dedicated schema for the project i.e analytics
-CREATE SCHEMA IF NOT EXISTS ANALYTICS;
--- retailmart_analytics_project_PgSQL/check.sql
+++
@@ -1,7 +0,0 @@
-select * from "analytics"."kpi_metadata";
-
---checking the first vire that is created in the sales analytics
-select * from analytics.vw_daily_sales_summary
-
---checking the materialized view 
-select * from analytics.mv_monthly_sales_dashboard
